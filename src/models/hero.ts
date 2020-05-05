2de315d32e6c40708cc796ba3082a0fe38222f10
poan <pibicha@163.com>
2020-05-05 16:00:12 +0800

    dva effects、subscriptions使用

--- src/models/hero.ts
+++ src/models/hero.ts
@@ -1,18 +1,42 @@
-import { Effect, Reducer } from 'umi';
-
-
-
+import { Effect, Reducer, Subscription, request } from 'umi';
 
 const HeroModel = {
   namespace: 'hero',
 
   state: {
     name: 'hero',
+    heros: [],
   },
 
   effects: {
-    *query({ payload }, { call, put }) {
+    *fetch({ type, payload }, { put, call, select }) {
+      console.log(111111);
 
+      const resp = yield request('/web201605/js/herolist.json');
+      const data = [
+        {
+          ename: 105,
+          cname: '廉颇',
+          title: '正义爆轰',
+          new_type: 0,
+          hero_type: 3,
+          skin_name: '正义爆轰|地狱岩魂',
+        },
+        {
+          ename: 106,
+          cname: '小乔',
+          title: '恋之微风',
+          new_type: 0,
+          hero_type: 2,
+          skin_name: '恋之微风|万圣前夜|天鹅之梦|纯白花嫁|缤纷独角兽',
+        },
+      ];
+      yield put({
+        type: 'save',
+        payload: {
+          heros: resp || data,
+        },
+      });
     },
   },
   reducers: {
@@ -21,6 +45,19 @@ const HeroModel = {
         ...state,
         ...action.payload,
       };
+    },
+  },
+  subscriptions: {
+    setup({ dispatch, history }) {
+      return history.listen(({ pathname, query }) => {
+        console.log(pathname);
+
+        if (pathname === '/hero') {
+          dispatch({
+            type: 'fetch',
+          });
+        }
+      });
     },
   },
 };
